d1ec7898a9bc76446a57ffad227e38abc5880624
Jinyoung Jang <jyjang@uengine.org>
2022-12-14 09:07:48 +0900

    commit

---
+++ common-api/src/main/java/human/resource/mgmt/event/EmployeeJoinedEvent.java
@@ -0,0 +1,14 @@
+package human.resource.mgmt.event;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class EmployeeJoinedEvent {
+
+    private Long id;
+    private String userId;
+    private String name;
+    private String email;
+}
